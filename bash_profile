38ce6d41321935a9bf644013f550380d18dfc67c
Josh Deaton <jdeaton@users.noreply.github.com>
2018-09-14 16:10:03 -0400

    New organization structure with basic system configuration
    - Updated symbolic link creator.
    - system/ folder contains configurations to setup $PATH, specify
      terminal aliases, and format terminal prompt.

---
+++ bash_profile
@@ -0,0 +1,6 @@
+export DF=${HOME}/dotfiles
+
+source ${DF}/system/alias
+source ${DF}/system/path
+source ${DF}/system/prompt-git
+source ${DF}/system/prompt
--- symlinks.sh
+++ symlinks.sh
@@ -1,2 +1,6 @@
+# Git
 ln -svF "${HOME}/dotfiles/git/gitconfig" "${HOME}/.gitconfig"
 ln -svF "${HOME}/dotfiles/git/gitignore" "${HOME}/.gitignore"
+
+# System
+ln -svF "${HOME}/dotfiles/bash_profile" "${HOME}/.bash_profile"
